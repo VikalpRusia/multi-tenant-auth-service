d573c9cb0307230ed76cef458c7324fe4a7ef1c5
Vikalp Rusia <vikalprusia@gmail.com>
2024-08-15 17:17:28 +0530

    handle db connection

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
+import logging
+import traceback
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Request
+from fastapi.responses import JSONResponse
+from sqlalchemy.exc import IntegrityError
+
 from apis import router
-
 
 app = FastAPI(docs_url="/")
 app.include_router(router)
+
+logger = logging.getLogger(__name__)
+# TODO: close connection in lifespan
+
+@app.exception_handler(IntegrityError)
+def handle_db_exception(request: Request, exception: IntegrityError) -> JSONResponse:
+    logger.error("".join(traceback.format_exception(exception)))
+    return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"message": str(exception)})
