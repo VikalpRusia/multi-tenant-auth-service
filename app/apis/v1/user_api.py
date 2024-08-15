165d9b922d905aae4b9db7b2847e618725475073
Vikalp Rusia <vikalprusia@gmail.com>
2024-08-16 01:39:40 +0530

    get refresh and access token

--- app/apis/v1/user_api.py
+++ app/apis/v1/user_api.py
@@ -1,8 +1,12 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi_restful.cbv import cbv
 
 from controllers.user_controller import UserController
 from db.database import get_db_session
+from schemas.token import Token
 from schemas.user import UserCreate, User
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -16,3 +20,13 @@ class UserAPI:
     @router.post("/")
     async def sign_up(self, user: UserCreate, db=Depends(get_db_session)) -> User:
         return await self.controller.save_user(user, db)
+
+    @router.post("/token")
+    async def login(
+        self,
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+        db=Depends(get_db_session),
+    ) -> Token:
+        return await self.controller.login_for_access_token(
+            form_data.username, form_data.password, db=db
+        )
--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,9 +1,22 @@
 import time
+from datetime import timedelta, datetime, timezone
 
+import jwt
+from bcrypt import checkpw
+from fastapi import HTTPException, status
+
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models import User
+from schemas.token import Token
 from schemas.user import UserCreate
+from config.constants import (
+    SECRET_KEY,
+    ALGORITHM,
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    REFRESH_TOKEN_EXPIRE_DAYS,
+)
 
 
 class UserController:
@@ -19,3 +32,53 @@ class UserController:
         await db.flush()
         await db.refresh(user_model)
         return user_model
+
+    @staticmethod
+    async def get_user(user_email, db: AsyncSession) -> User:
+        user = await db.scalar(select(User).where(User.email == user_email))
+        return user
+
+    @staticmethod
+    async def validate_user(
+        user_email: str, user_password: str, db: AsyncSession
+    ) -> bool:
+        user_in_db = await UserController.get_user(user_email, db)
+        if user_in_db is None:
+            return False
+        if not checkpw(
+            password=user_password.encode("utf-8"),
+            hashed_password=user_in_db.password.encode("utf-8"),
+        ):
+            return False
+        return True
+
+    @staticmethod
+    async def create_token(
+        data: dict,
+        expires_delta: timedelta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
+    ) -> str:
+        to_encode = data.copy()
+        expire = datetime.now(tz=timezone.utc) + expires_delta
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+        return encoded_jwt
+
+    @staticmethod
+    async def login_for_access_token(
+        email: str, password: str, db: AsyncSession
+    ) -> Token:
+        if not await UserController.validate_user(email, password, db):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+            )
+        access_token = await UserController.create_token(
+            data={"sub": email, "type": "access_token"}
+        )
+        refresh_token = await UserController.create_token(
+            data={"sub": email, "type": "refresh_token"},
+            expires_delta=timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS),
+        )
+        return Token(
+            access_token=access_token, refresh_token=refresh_token, token_type="bearer"
+        )
