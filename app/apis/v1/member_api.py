31dc818f3d25a05dc02aed2fc3dffa93ef629417
Vikalp Rusia <vikalprusia@gmail.com>
2024-08-16 17:04:21 +0530

    add delete

--- app/apis/v1/member_api.py
+++ app/apis/v1/member_api.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi_restful.cbv import cbv
 
 from controllers.member_controller import MemberController
 from db.database import get_db_session
-from schemas.member import MemberCreate, Member
+from schemas.member import MemberCreate, Member, UpdateMember
 
 router = APIRouter(prefix="/member", tags=["Member"])
 
@@ -16,3 +16,9 @@ class MemberAPI:
     @router.post("/")
     async def sign_up(self, member: MemberCreate, db=Depends(get_db_session)) -> Member:
         return await self.member_controller.save_member(member, db)
+
+    @router.delete("/{member_id}")
+    async def delete_member(self, member_id: int, db=Depends(get_db_session)):
+        if await self.member_controller.delete_member(member_id, db):
+            return {"message": "deleted"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Member doesn't exist to be deleted")
--- app/controllers/member_controller.py
+++ app/controllers/member_controller.py
@@ -1,5 +1,6 @@
 import time
 
+from sqlalchemy import delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models import Member
@@ -17,3 +18,8 @@ class MemberController:
         await db.flush()
         await db.refresh(member_model)
         return member_model
+
+    @staticmethod
+    async def delete_member(member_id: int, db: AsyncSession):
+        result = await db.execute(delete(Member).where(member_id == Member.id))
+        return result.rowcount==1
